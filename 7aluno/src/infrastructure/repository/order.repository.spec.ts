cff78b64ad885e666c87856a19d39e9089ae80be
engendromestre <engendro.mestre@gmail.com>
2024-01-24 12:18:05 -0300

    Desafio dos repositórios-find e findAll

--- 7aluno/src/infrastructure/repository/order.repository.spec.ts
+++ 7aluno/src/infrastructure/repository/order.repository.spec.ts
@@ -168,4 +168,89 @@ describe("Order repository test", () => {
       ],
     });
   });
+
+  it("should find a order", async () => {
+    const customerRepository = new CustomerRepository();
+    const customer = new Customer("123", "Customer 1");
+    const address = new Address("Street 1", 1, "Zipcode 1", "City 1");
+    customer.changeAddress(address);
+    await customerRepository.create(customer);
+
+    const productRepository = new ProductRepository();
+    const product = new Product("123", "Product 1", 10);
+    await productRepository.create(product);
+
+    const orderItem = new OrderItem(
+      "1",
+      product.name,
+      product.price,
+      product.id,
+      2
+    );
+
+    const order = new Order("123", "123", [orderItem]);
+
+    const orderRepository = new OrderRepository();
+    await orderRepository.create(order);
+
+    const orderResult = await orderRepository.find(order.id);
+
+    expect(order).toStrictEqual(orderResult);
+  });
+
+  it("should find all orders", async () => {
+    const customerRepository = new CustomerRepository();
+    const customer = new Customer("123", "Customer 1");
+    const address = new Address("Street 1", 1, "Zipcode 1", "City 1");
+    customer.changeAddress(address);
+    await customerRepository.create(customer);
+
+    const productRepository = new ProductRepository();
+    const product = new Product("123", "Product 1", 10);
+    await productRepository.create(product);
+
+    const orderItem = new OrderItem(
+      "1",
+      product.name,
+      product.price,
+      product.id,
+      2
+    );
+
+    const order = new Order("123", customer.id, [orderItem]);
+
+    const orderRepository = new OrderRepository();
+    await orderRepository.create(order);
+
+    const customer2 = new Customer("124", "Customer 2");
+    customer2.changeAddress(address);
+    await customerRepository.create(customer2);
+    const product2 = new Product("124", "Product 2", 20);
+    await productRepository.create(product2);
+    const orderItem2 = new OrderItem(
+      "2",
+      product2.name,
+      product2.price,
+      product2.id,
+      3
+    );
+
+    const product3 = new Product("125", "Product 3", 30);
+    await productRepository.create(product3);
+
+    const orderItem3 = new OrderItem(
+      "3",
+      product3.name,
+      product3.price,
+      product3.id,
+      3
+    );
+    const order2 = new Order("124", customer2.id, [orderItem2, orderItem3]);
+    await orderRepository.create(order2);
+
+    const orders = await orderRepository.findAll();
+    expect(orders).toHaveLength(2);
+    expect(orders).toContainEqual(order);
+    expect(orders).toContainEqual(order2);
+  });
 });
--- 7aluno/src/infrastructure/repository/order.respository.ts
+++ 7aluno/src/infrastructure/repository/order.respository.ts
@@ -2,7 +2,7 @@ import Order from "../../domain/entity/order";
 import OrderItemModel from "../db/orm/sequelize/model/order-item.model";
 import OrderModel from "../db/orm/sequelize/model/order.model";
 import OrderRepositoryInterface from "../../domain/repository/order-repository.interface";
-import { Model } from "sequelize";
+import OrderItem from "../../domain/entity/order_item";
 
 export default class OrderRepository implements OrderRepositoryInterface {
   async create(entity: Order): Promise<void> {
@@ -51,10 +51,50 @@ export default class OrderRepository implements OrderRepositoryInterface {
   }
 
   async find(id: string): Promise<Order> {
-    throw new Error("Unable to change an order");
+    let orderModel;
+    try {
+      orderModel = await OrderModel.findOne({
+        where: {
+          id,
+        },
+        include: ["items"],
+        rejectOnEmpty: true,
+      });
+    } catch (error) {
+      throw new Error("Order not found");
+    }
+
+    const arrItems = this.getOrderItems(orderModel.toJSON().items);
+    const order = new Order(id, orderModel.customer_id, arrItems);
+    return order;
   }
 
   async findAll(): Promise<Order[]> {
-    throw new Error("Unable to change an order");
+    const orderModels = await OrderModel.findAll({
+      include: ["items"],
+    });
+
+    const orders = orderModels.map((orderModel) => {
+      const arrItems = this.getOrderItems(orderModel.toJSON().items);
+      let order = new Order(orderModel.id, orderModel.customer_id, arrItems);
+      return order;
+    });
+    return orders;
+  }
+
+  getOrderItems(items: OrderItemModel[]): OrderItem[] {
+    const arrItems: OrderItem[] = [];
+    for (let i = 0; i < items.length; i++) {
+      let itemModel = items[i];
+      let orderItem = new OrderItem(
+        itemModel.id,
+        itemModel.name,
+        itemModel.price,
+        itemModel.product_id,
+        itemModel.quantity
+      );
+      arrItems.push(orderItem);
+    }
+    return arrItems;
   }
 }
